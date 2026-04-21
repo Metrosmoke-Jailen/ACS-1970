d89c1510835e43fb49b5dfbd8bd6e56fea17badc
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2026-04-21 16:01:48 -0700

    Still fiddling with the reviews page

--- frontend/src/Fields/Movie/Movies.jsx
+++ frontend/src/Fields/Movie/Movies.jsx
@@ -18,48 +18,45 @@ function Movies() {
   return (
     <div className="Movies">
 
-      {/* MEDIA HEADER BLOCK */}
-      <div className="mediaHeaderBlock">
+      {/* QUADRANT LAYOUT */}
+      <div className="moviesGrid">
 
-        <ControlBar
-          field={'movie'}
-          queryPlaceHolder={'Search for a movie'}
-        />
-
-        <MediaOverview
-          title='Project Hail Mary'
-          description="Science teacher Ryland Grace wakes up on a 
-              spaceship with no recollection of who he is or how he got there. 
-              As his memory slowly returns, he soon discovers he must solve 
-              the riddle behind a mysterious substance that' s causing the sun 
-              to die out. As details of the mission unravel, he calls on his 
-              scientific training and sheer ingenuity -- but he may not have 
-              to do it alone."
-          poster='/Untitled.jpg'
-          cast={["Ryan Gosling", "Phil Lord", "Chris Miller",
-            "Amy Pascal", "Aditya Sood", "Andy Weir", "Rachel O'Connor"]}
-        />
-
-      </div>
-
-      {/* BODY LAYOUT */}
-      <div className="moviesContent">
-
-        {/* LEFT / MAIN INFO STACK */}
-        <div className="moviesLeft">
-
-          <MediaDetails />
-          <MediaReviews reviews={movieReviews} />
-
+        {/* TOP-LEFT: CONTROL BAR + MEDIA OVERVIEW */}
+        <div className="quadrant topLeft">
+          <ControlBar
+            field={'movie'}
+            queryPlaceHolder={'Search for a movie'}
+          />
+          <MediaOverview
+            title='Project Hail Mary'
+            description="Science teacher Ryland Grace wakes up on a 
+                spaceship with no recollection of who he is or how he got there. 
+                As his memory slowly returns, he soon discovers he must solve 
+                the riddle behind a mysterious substance that' s causing the sun 
+                to die out. As details of the mission unravel, he calls on his 
+                scientific training and sheer ingenuity -- but he may not have 
+                to do it alone."
+            poster='/Untitled.jpg'
+            cast={["Ryan Gosling", "Phil Lord", "Chris Miller",
+              "Amy Pascal", "Aditya Sood", "Andy Weir", "Rachel O'Connor"]}
+          />
         </div>
 
-        {/* RIGHT / NPS PANEL */}
-        <div className="moviesRight">
-
+        {/* TOP-RIGHT: NPS SCORES + ACTIONS */}
+        <div className="quadrant topRight">
           <NPSScore score={78} />
           <UserNPSScore />
           <ActionPanel />
+        </div>
 
+        {/* BOTTOM-LEFT: MEDIA DETAILS */}
+        <div className="quadrant bottomLeft">
+          <MediaDetails />
+        </div>
+
+        {/* BOTTOM-RIGHT: COMMENTS/REVIEWS */}
+        <div className="quadrant bottomRight">
+          <MediaReviews reviews={movieReviews} />
         </div>
 
       </div>
