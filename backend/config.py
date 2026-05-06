d847292fa6d2635870398d3cbcf6aa3d54620822
kentongelis <kenton.gelis@students.dominican.edu>
2026-05-06 15:07:41 -0700

    render setup

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,5 @@
+import os
 from pathlib import Path
 
-DB_PATH = Path(__file__).with_name("movies.db")
+DB_PATH = Path(os.environ.get("DB_PATH", Path(__file__).with_name("movies.db")))
 DATABASE_URL = f"sqlite:///{DB_PATH}"
